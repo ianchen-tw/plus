34bfd8a2e71eeb1bc046c787f7804b9ea0898dcc
Ian Chen <ianre657@gmail.com>
2020-07-13 12:08:04 +0000

    [Infra] add vscode:remote-container support

--- plus/scripts/.format.sh
+++ plus/scripts/.format.sh
@@ -1,13 +1,14 @@
 #! /bin/sh
+# This file should be runned in container
 
 # Sort imports one per line, so autoflake can remove unused imports
-isort --force-single-line-imports app
+isort --force-single-line-imports /app
 
 # remove unused imports
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app --exclude=__init__.py
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place /app --exclude=__init__.py
 
 # sort and gather imports
-isort app
+isort /app
 
 # formatter
-black app
+black /app
