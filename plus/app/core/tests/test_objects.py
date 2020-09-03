a56f347872fbe5ab3faa7171d24b256c098e9e26
Ian Chen <ianre657@gmail.com>
2020-09-03 23:36:37 +0800

    create CodedTimeInterval obejct

---
+++ plus/app/core/tests/test_objects.py
@@ -0,0 +1,35 @@
+from typing import Generator
+
+import pytest
+from attr.exceptions import FrozenInstanceError
+
+from ..objects import CodedTimeInterval
+
+
+@pytest.fixture(scope="module")
+def interval_arg() -> Generator:
+    yield {"code": "A", "weekday": "Fri", "timespan": "8:00-9:00", "kind": "nctu"}
+
+
+@pytest.mark.dev
+def test_interval_dict(interval_arg):
+    interval = CodedTimeInterval(**interval_arg)
+    assert interval.as_dict() == interval_arg
+
+
+@pytest.mark.dev
+def test_interval_is_immutable(interval_arg):
+    interval = CodedTimeInterval(**interval_arg)
+    with pytest.raises(FrozenInstanceError):
+        interval.code = "Nah"
+
+
+@pytest.mark.dev
+def test_interval_comparison():
+    i1 = CodedTimeInterval(code="A", weekday="Fri", timespan="8:00-9:00", kind="nctu")
+    i2 = CodedTimeInterval(code="A", weekday="Fri", timespan="8:00-9:00", kind="nctu")
+    assert i1 == i2
+
+    i3 = CodedTimeInterval(code="A", weekday="Fri", timespan="8:00-9:00", kind="nctu")
+    i4 = CodedTimeInterval(code="B", weekday="Mon", timespan="8:00-9:00", kind="nctu")
+    assert i3 != i4
