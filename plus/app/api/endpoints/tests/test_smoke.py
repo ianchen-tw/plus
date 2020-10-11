f580ae18e8e0f4b6832ddbe55c0419886a03e834
Ian Chen <ianre657@gmail.com>
2020-10-11 14:17:11 +0800

    Revert "Ci smoke test (#50)"
    This reverts commit c3e7017d188ff36e4682d95569a97e93aae13101.

--- plus/app/api/endpoints/tests/test_smoke.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-
-@pytest.mark.smoke
-def test_backend_start_success(client):
-    res = client.get("/")
-    assert res.status_code == 200
--- plus/scripts/.smoke_test.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-# This file should be runned in container
-
-pytest --pyargs /app -m smoke || (docker-compose --timestamps logs && exit 1)
